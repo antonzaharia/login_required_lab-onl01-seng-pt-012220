799e4e20e948ce50f9d7a4f77a11aebd54fcece9
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-19 16:26:21 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'sessions#index'
-  get '/login', to: 'sessions#login'
+  get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/secret', to: 'secrets#show'
 end
