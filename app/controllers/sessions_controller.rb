6d01adaf1946eef8f69bf10df509f12a2d6bb2d6
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-19 16:35:52 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   before_action :require_login
-  skip_before_action :require_login, only: [:new, :create]
+  skip_before_action :require_login, only: [:new]
 
   def index
     @name = session[:name]
@@ -10,10 +10,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    if params[:name] == ""
+      redirect_to login_path
+    else
       session[:name] = params[:name]
       redirect_to root_path
-
+    end
   end
 
   def destroy
