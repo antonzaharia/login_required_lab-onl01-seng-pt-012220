e490b47161c68011626735d45317ece2e96b961d
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-19 16:14:53 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,4 +1,10 @@
 class SecretsController < ApplicationController
+  before_action :
   def show
+    @secret = "This is the secret."
   end
+
+  private
+  def require_login
+    return head(:forbidden) unless session.include?(:name)
 end
