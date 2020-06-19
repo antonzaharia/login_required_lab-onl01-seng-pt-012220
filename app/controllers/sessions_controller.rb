9e6e14bdb1b922157ca3cd54ab3740e49f5a85a3
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-19 16:35:10 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,12 +10,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if params[:name] == ""
-      redirect_to login_path
-    else
+
       session[:name] = params[:name]
       redirect_to root_path
-    end
+
   end
 
   def destroy
