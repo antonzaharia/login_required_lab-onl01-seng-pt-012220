37cc5f7037d1e64ab367aa187fc0bba2ffd86e5f
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-19 16:31:25 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   before_action :require_login
-  skip_before_action :require_login, only: [:login, :index]
+  skip_before_action :require_login, only: [:login]
 
   def index
     @name = session[:name]
